33fa93dcd73511c7ca6eff5a2d7f928c20c5515c
Adam Shur <adamswebventures@gmail.com>
2020-03-08 21:26:49 -0400

    trying to get CMS to grab the right posts folder

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,6 +11,7 @@ module.exports = {
     {
       resolve: `@lekoarts/gatsby-theme-minimal-blog`,
       options: {
+        postsPath: `content/posts`,
         navigation: [
           {
             title: `Blog`,
